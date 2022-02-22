e60f0670b90d30adb084161f8fb3821fe093aa65
Preetpal <w3irdg@gmail.com>
2022-02-22 03:43:08 -0500

    Added logging functions

--- app.py
+++ app.py
@@ -54,4 +54,7 @@ def insert_new_question():
 
 
 if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     app.run(host='0.0.0.0')
