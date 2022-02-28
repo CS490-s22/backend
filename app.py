9b23d8e0c1d628736e2f0418fd8fff5cb952a11b
Preet S <w3irdg@gmmail.com>
2022-02-27 19:14:33 -0500

    Using Blueprints now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from questions import questions
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from db_cred import db #creds
@@ -15,6 +16,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+app.register_blueprint(questions)
 
 @app.route('/', methods=['GET', 'POST'])
 def start_app():
@@ -51,66 +53,6 @@ def validate_login():
     else:
         return jsonify(error = "Content-Type not supported | Request must be in JSON format")
     
-# -----------------------
-# TEACHER FUNCTIONALITIES
-# -----------------------
-# Retreive Question Bank or questions based on request method
-@app.route('/question_bank', methods=['GET','POST'])
-def retreive_questions():
-    cur = mysql.connection.cursor()
-    if request.method == "GET":
-        rows = cur.execute("SELECT id, title, topics AS 'category', question AS description, difficulty, madeby FROM questions")
-        if rows > 0:
-            result = cur.fetchall()
-            return jsonify(result)
-    elif request.method == "POST":
-        content_type = request.headers.get('Content-Type')
-        if content_type == "application/json":
-            req = request.json
-            limit = req['limit']
-            rows = cur.execute(f"SELECT id, title, topics AS 'category', question AS description, difficulty, madeby FROM questions LIMIT {limit}")
-            if rows > 0:
-                result = cur.fetchall()
-            return jsonify(result)
-        else:
-            return jsonify(error = "Content-Type not supported | Request must be in JSON format")
-    else:
-        return jsonify(error="Howdidyougethere?")
-
-#Insert new question into question bank
-@app.route('/new_question', methods=['POST'])
-def insert_new_question():
-    cur = mysql.connection.cursor()
-    if request.method != 'POST':
-        return jsonify(error="REQUIRES POST REQUEST")
-    
-    content_type = request.headers.get("Content-Type")
-    if content_type == 'application/json':
-        req = request.json
-        title = req['title']
-        topic = req['category']
-        difficulty = req['difficulty']
-        question = req['description']
-        madeby = req['professorID']
-        testcases = req['testCases']
-
-        rows_affected = cur.execute("INSERT INTO questions(id, title, topics, question, difficulty, madeby) VALUES(null,\"{}\",\"{}\",\"{}\",\"{}\",{})".format(title,topic,question,difficulty,madeby))
-        mysql.connection.commit()
-        logging.warn("ROWS INSERTED INTO QUESTIONS: %d", rows_affected)
-
-        cur.execute("SELECT MAX(id) as id FROM questions")
-        question_id = cur.fetchall()[0]['id']
-        
-        for case in testcases:
-            caseI = case['input']
-            caseO = case['output']
-            rows_affected = cur.execute("INSERT INTO testcases(id, qid, input, output) VALUES(null,{},'{}','{}')".format(question_id,caseI,caseO))
-            mysql.connection.commit()
-            logging.warn("ROWS INSERTED INTO TESTCASES: %d", rows_affected)
-        
-        return jsonify(result="OK", questionID = question_id)
-    else:
-        return jsonify(error="JSON FORMAT REQUIRED")
 
 #Create new exam
 @app.route('/new_exam',methods=['POST'])
---
+++ questions.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, jsonify, request, current_app as app
+import logging
+from flask_mysqldb import MySQL
+
+questions = Blueprint("questions",__name__)
+
+mysql = MySQL(app)
+
+# -----------------------
+# TEACHER FUNCTIONALITIES
+# -----------------------
+# Retreive Question Bank or questions based on request method
+@questions.route('/question_bank', methods=['GET','POST'])
+def retreive_questions():
+    cur = mysql.connection.cursor()
+    if request.method == "GET":
+        rows = cur.execute("SELECT id, title, topics AS 'category', question AS description, difficulty, madeby FROM questions")
+        if rows > 0:
+            result = cur.fetchall()
+            return jsonify(result)
+    elif request.method == "POST":
+        content_type = request.headers.get('Content-Type')
+        if content_type == "application/json":
+            req = request.json
+            limit = req['limit']
+            rows = cur.execute(f"SELECT id, title, topics AS 'category', question AS description, difficulty, madeby FROM questions LIMIT {limit}")
+            if rows > 0:
+                result = cur.fetchall()
+            return jsonify(result)
+        else:
+            return jsonify(error = "Content-Type not supported | Request must be in JSON format")
+    else:
+        return jsonify(error="Howdidyougethere?")
+
+#Insert new question into question bank
+@questions.route('/new_question', methods=['POST'])
+def insert_new_question():
+    cur = mysql.connection.cursor()
+    if request.method != 'POST':
+        return jsonify(error="REQUIRES POST REQUEST")
+    
+    content_type = request.headers.get("Content-Type")
+    if content_type == 'application/json':
+        req = request.json
+        title = req['title']
+        topic = req['category']
+        difficulty = req['difficulty']
+        question = req['description']
+        madeby = req['professorID']
+        testcases = req['testCases']
+
+        rows_affected = cur.execute("INSERT INTO questions(id, title, topics, question, difficulty, madeby) VALUES(null,\"{}\",\"{}\",\"{}\",\"{}\",{})".format(title,topic,question,difficulty,madeby))
+        mysql.connection.commit()
+        logging.warn("ROWS INSERTED INTO QUESTIONS: %d", rows_affected)
+
+        cur.execute("SELECT MAX(id) as id FROM questions")
+        question_id = cur.fetchall()[0]['id']
+        
+        for case in testcases:
+            caseI = case['input']
+            caseO = case['output']
+            rows_affected = cur.execute("INSERT INTO testcases(id, qid, input, output) VALUES(null,{},'{}','{}')".format(question_id,caseI,caseO))
+            mysql.connection.commit()
+            logging.warn("ROWS INSERTED INTO TESTCASES: %d", rows_affected)
+        
+        return jsonify(result="OK", questionID = question_id)
+    else:
+        return jsonify(error="JSON FORMAT REQUIRED")
